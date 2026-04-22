e66df912e35f6031893f4eff44a8140498c7ea75
Shabari Rust <amitairobo@gmail.com>
2026-04-22 21:23:30 +0530

    Add files via upload

--- app/src/main/java/car1/RustLogic.kt
+++ app/src/main/java/car1/RustLogic.kt
@@ -2,17 +2,21 @@ package car1
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import rust.interop.bridge.*
+import rust.interop.bridge.FilterParams
+import rust.interop.bridge.FilterResponse
+import rust.interop.bridge.fetchInteroperability
+
+val params = FilterParams(
+    integration = null,
+    developmentkit = null,
+    language = null,
+    crates = null,
+    page = "1",
+    ids = null
+)
 
 suspend fun fetchDataFromRust(pageNumber: Int): FilterResponse {
-    val params = FilterParams(
-        integration = null,
-        developmentkit = null,
-        language = null,
-        crates = null,
-        page = pageNumber.toString(),
-        ids = null
-    )
+    params.page = pageNumber.toString();
     return withContext(Dispatchers.IO) {
         fetchInteroperability(params)
     }
